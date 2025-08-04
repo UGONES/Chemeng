efab460b64e95a830d025db78dcaa462c3cb8513
UGONES <ugochukwuhumphreyezeamaku@gmail.com>
2025-08-04 23:04:14 +0100

    since the vercel.com is giving me hard time i used render and fixed the react issues

--- src/index.js
+++ src/index.js
@@ -3,20 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "./i18n";
+import "./i18n"; // this must register i18n config
+import i18n from './i18n'; // <-- import the actual instance
 import { I18nextProvider } from 'react-i18next';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <I18nextProvider>
+    <I18nextProvider i18n={i18n}>  {/* ✅ fixed */}
       <App />
     </I18nextProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
